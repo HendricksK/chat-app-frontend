616a510fdbe130b041f9db0611d3a542909507eb
HendricksK <kurvinhendricks@gmail.com>
2020-05-31 21:48:20 +0200

    Appjs update, added new components added yarnjs for SCSS implementation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
 // App.js
 import React, { Component } from "react";
+import Header from './components/Header/Header';
+import ChatHistory from './components/ChatHistory/ChatHistory';
+
 import "./App.css";
 import { connect, sendMsg } from "./api";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    connect();
-  }
 
-  send() {
-    sendMsg("hello is it me you are looking for?");
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			chatHistory: [
+				"hello there",
+				"is it me you are looking for?"
+			]
+		}
+	}
 
-  render() {
-    return (
-        <div className="App">
-          <button onClick={this.send}>Hit</button>
-        </div>
-    );
-  }
+	componentDidMount() {
+		connect((msg) => {
+			console.log("New message")
+			this.setState(prevState => ({
+				chatHistory: [...this.state.chatHistory, msg]
+			}))
+			console.log(this.state);
+		});
+	}
+
+	send(event) {
+		sendMsg(event.target.value);
+	}
+
+	render() {
+		return (
+				<div className="App">
+					<Header />
+					<ChatHistory chatHistory={this.state.chatHistory} />
+					<button onClick={this.send}>Hit</button>
+				</div>
+		);
+	}
 }
 
 export default App;
