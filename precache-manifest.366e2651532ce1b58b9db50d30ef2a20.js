2d4a99a2fe2e04e4967d2d5f51e8ac2438a7786e
HendricksK <kurvinhendricks@gmail.com>
2020-06-05 22:55:59 +0200

    Updates

---
+++ precache-manifest.366e2651532ce1b58b9db50d30ef2a20.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "576d2a4858f563f9327f13f1713ce746",
+    "url": "/chat-app-frontend/index.html"
+  },
+  {
+    "revision": "8c0b51f82b69f3c085e3",
+    "url": "/chat-app-frontend/static/css/main.f6f82d9d.chunk.css"
+  },
+  {
+    "revision": "b233f348a57cbcab408f",
+    "url": "/chat-app-frontend/static/js/2.96baa305.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/chat-app-frontend/static/js/2.96baa305.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c0b51f82b69f3c085e3",
+    "url": "/chat-app-frontend/static/js/main.dab69645.chunk.js"
+  },
+  {
+    "revision": "8869ec1a4a9d35a7ba0f",
+    "url": "/chat-app-frontend/static/js/runtime-main.037f9916.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.aa21c3586be0e1b6ebde13cf1f34261c.js"
+  "/chat-app-frontend/precache-manifest.366e2651532ce1b58b9db50d30ef2a20.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/chat-app-frontend/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
